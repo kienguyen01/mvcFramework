0f72107df2461857da77c037f11c33e0bbc8028b
kienguyen01 <54843446+kienguyen01@users.noreply.github.com>
2021-01-09 00:12:10 +0700

    Add files via upload

---
+++ mvc/app/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+//load config
+require_once 'config/config.php';
+//Load library
+//require_once 'libraries/Core.php';
+//require_once 'libraries/Controller.php';
+//require_once 'libraries/Database.php';
+
+
+
+//Autoload core libraries
+spl_autoload_register(function($className){
+    require_once 'libraries/'.$className.'.php';
+
+});
---
+++ mvc/app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+    //DB params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', '_YOUR_USER');
+    define('DB_PASS', '');
+    define('DB_NAME', '_YOUR_DBNAME');
+    //App root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // URL Root
+
+    define('URLROOT', '_YOUR_URL_');
+    //Site name
+    define('SITENAME', '_YOUR_SITENAME_');
